672434e375a99efb9c1496f0e225ca13c9779e8e
Fernando-Kuma <fernando998@outlook.es>
2023-04-05 12:43:04 -0600

    Se agrega bootstrap

--- src/app/shared/utils/header-home/header-home.component.ts
+++ src/app/shared/utils/header-home/header-home.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { NAV } from '../../configuration/navegacion';
 
 @Component({
   selector: 'app-header-home',
@@ -10,9 +12,33 @@ export class HeaderHomeComponent implements OnInit {
   @Input() color: string = '#122D4F';
   @Input() logo: string = 'image-toolbar';
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  cambiarMenu(numeroMenu:number){
+    switch (numeroMenu) {
+      case 1:
+          localStorage.setItem('menu', '1');
+          this.router.navigateByUrl(NAV.dashboard);
+        break;
+      case 2:
+          localStorage.setItem('menu', '2');
+          this.router.navigateByUrl(NAV.proceso);
+        break;
+      default:
+        break;
+    }
+  }
+
+  salir(){
+    localStorage.clear();
+    this.router.navigateByUrl(NAV.login);
+  }
+
+  abrirModuloAyuda(){
+    this.router.navigateByUrl(NAV.login);
   }
 
 }

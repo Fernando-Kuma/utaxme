5f9db8aea30aedba2251db028a4857c9fbf70487
Fernando-Kuma <fernando998@outlook.es>
2023-04-11 12:16:48 -0600

    se agrego el datePicker al dasboard

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -6,19 +6,35 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { HeaderHomeModule } from 'src/app/shared/utils/header-home';
 import { HomeComponent } from './home.component';
 import { GraphicsModule } from 'src/app/graphics/graphics.module';
-
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+/* import {MatNativeDateModule} from '@angular/material';
+import { MatMomentDateModule } from "@angular/material/datepicker"; */
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    HomeComponent
+    HomeComponent,
+    
   ],
   imports: [
     CommonModule,
     RouterModule,
     GraphicsModule,
-    HeaderHomeModule
+    HeaderHomeModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule, 
+    MatInputModule
+  ],
+  exports: [
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule, 
+    MatInputModule
   ]
 })
 export class HomeModule { }

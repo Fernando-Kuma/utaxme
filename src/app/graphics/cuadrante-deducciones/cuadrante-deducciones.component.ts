1a4b2760884cb1ed252bdd4a2d632b4cdf6a00c0
Fernando-Kuma <fernando998@outlook.es>
2023-04-12 13:16:29 -0600

    se agrego paginador a la tabla de deducciones

--- src/app/graphics/cuadrante-deducciones/cuadrante-deducciones.component.ts
+++ src/app/graphics/cuadrante-deducciones/cuadrante-deducciones.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Paginator } from 'array-paginator';
 
 export interface PeriodicElement {
   deduccion: string;
@@ -13,6 +14,18 @@ const ELEMENT_DATA: PeriodicElement[] = [
   {clave: 'D03', deduccion: 'Donativos', requisitos: 'Debe ser emitido por una donataria autorizada', detalle: 'Li'},
   {clave: 'D04', deduccion: 'Intereses reales efectivamente pagados por créditos hipotecarios ', requisitos: 'Deben ser realizados antes de presentar la declaración anual', detalle: 'Be'},
   {clave: 'D05', deduccion: 'Aportaciones voluntarias SAR', requisitos: 'Deben ser realizados directamente a su subcuenta', detalle: 'B'},
+  {clave: 'D06', deduccion: 'Honorarios médicos, dentales y gastos hospitalarios', requisitos: 'Que sea pagada con un medio electrónico', detalle: 'H'},
+  {clave: 'D07', deduccion: 'Gastos funerarios', requisitos: 'No incluye gastos funerarios futuros', detalle: 'He'},
+  {clave: 'D08', deduccion: 'Donativos', requisitos: 'Debe ser emitido por una donataria autorizada', detalle: 'Li'},
+  {clave: 'D09', deduccion: 'Intereses reales efectivamente pagados por créditos hipotecarios ', requisitos: 'Deben ser realizados antes de presentar la declaración anual', detalle: 'Be'},
+  {clave: 'D010', deduccion: 'Aportaciones voluntarias SAR', requisitos: 'Deben ser realizados directamente a su subcuenta', detalle: 'B'},
+  {clave: 'D011', deduccion: 'Intereses reales efectivamente pagados por créditos hipotecarios ', requisitos: 'Deben ser realizados antes de presentar la declaración anual', detalle: 'Be'},
+  {clave: 'D012', deduccion: 'Aportaciones voluntarias SAR', requisitos: 'Deben ser realizados directamente a su subcuenta', detalle: 'B'},
+  {clave: 'D013', deduccion: 'Honorarios médicos, dentales y gastos hospitalarios', requisitos: 'Que sea pagada con un medio electrónico', detalle: 'H'},
+  {clave: 'D014', deduccion: 'Gastos funerarios', requisitos: 'No incluye gastos funerarios futuros', detalle: 'He'},
+  {clave: 'D015', deduccion: 'Donativos', requisitos: 'Debe ser emitido por una donataria autorizada', detalle: 'Li'},
+  {clave: 'D016', deduccion: 'Intereses reales efectivamente pagados por créditos hipotecarios ', requisitos: 'Deben ser realizados antes de presentar la declaración anual', detalle: 'Be'},
+  {clave: 'D017', deduccion: 'Aportaciones voluntarias SAR', requisitos: 'Deben ser realizados directamente a su subcuenta', detalle: 'B'},
 ];
 
 @Component({
@@ -22,12 +35,23 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class CuadranteDeduccionesComponent implements OnInit {
 
+  listaResultado = ELEMENT_DATA;
   displayedColumns: string[] = ['clave', 'deduccion', 'requisitos', 'detalle'];
-  dataSource = ELEMENT_DATA;
-
+  public pager:any;
 
   constructor() { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void { 
+    this.paginador(this.listaResultado);
+  }
+
+   onPaged(page) {
+    this.listaResultado = this.pager.page(page);
+  }
+
+  paginador(value: any){
+    this.pager = new Paginator(value,5,1)
+    this.onPaged(1)
+  }
 
 }
